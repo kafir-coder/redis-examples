971aab7a10eb6600665c6ce27e209c9e1a50936a
Caio Tony <caiotony16@gmail.com>
2021-10-27 18:30:27 +0100

    refactor: add key and value constants

--- strings/example#1.js
+++ strings/example#1.js
@@ -1,10 +1,15 @@
 const IOredis = require('ioredis');
 const redis_connection = new IOredis();
 
+const KEY = "my-key";
+const VALUE = "my-value";
+
 const set_sds = async (key = "", value = "") => {
   const request = await redis_connection.set(key, value);
   return request;
 }
+
+
 
 set_sds()
   .then(result => {
@@ -15,3 +20,4 @@ set_sds()
     console.log(reason)
     process.exit(0)
   });
+
